1699705fa1a6495082e48c42bac32e6ba5069d23
TAGOMORI Satoshi <tagomoris@gmail.com>
2017-08-23 11:26:27 +0900

    remove unused import

--- src/test/java/com/treasuredata/bigdam/log/LogTest.java
+++ src/test/java/com/treasuredata/bigdam/log/LogTest.java
@@ -22,8 +22,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import com.treasuredata.bigdam.pool.UniqueId;
-
 public class LogTest
 {
     @After
